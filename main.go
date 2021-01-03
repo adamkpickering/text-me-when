a9fc028a37888dbb25132481b4b698f828ba9a9a
Adam Pickering <adamkpickering@gmail.com>
2021-01-02 19:29:23 -0700

    Added logging to main.go

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"time"
-	"fmt"
-	"flag"
-	"io/ioutil"
 	"encoding/json"
-	"path/filepath"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
 
 	"github.com/adamkpickering/reminder-boi/reminder"
@@ -19,36 +20,38 @@ import (
 
 func send_message(sns_client *sns.SNS, message string, number string) error {
 	pi := &sns.PublishInput{
-		Message: &message,
+		Message:     &message,
 		PhoneNumber: &number,
 	}
 	if err := pi.Validate(); err != nil {
-		return fmt.Errorf("send_message: %w", err)
+		return fmt.Errorf("pi.Validate: %w", err)
 	}
-	fmt.Printf("sending msg \"%s\" to \"%s\"\n", message, number)
 	_, err := sns_client.Publish(pi)
 	if err != nil {
-		return fmt.Errorf("send_message: %w", err)
+		return fmt.Errorf("sns_client.Publish: %w", err)
 	}
 	return nil
 }
 
 func fire_reminders(call_time time.Time, sns_client *sns.SNS, reminder_list []reminder.ReminderV1) {
-	test_message := "this is a test message"
-	test_number := "+12345678901"
+	message := "this is a test message"
+	phone_number := "+12345678901"
 	for _, reminder := range reminder_list {
 		if reminder.ShouldRun(call_time) {
-			err := send_message(sns_client, test_message, test_number)
-			fmt.Printf("sending message \"%s\" to %s...", test_message, test_number)
+			err := send_message(sns_client, message, phone_number)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "fire_reminders: %w", err)
+				log.Printf("send_message failed: %w", err)
+				continue
 			}
-			fmt.Printf("message send successful\n")
+			log.Printf("Sent message \"%s\" to %s", message, phone_number)
 		}
 	}
 }
 
 func main() {
+	// set up logging
+	log.SetOutput(os.Stdout)
+
 	// parse CLI flags
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "%s [OPTIONS]\n\nOptions:\n", os.Args[0])
@@ -59,8 +62,8 @@ func main() {
 
 	// construct sns client
 	home_dir, ok := os.LookupEnv("HOME")
-	if ! ok {
-		fmt.Fprintf(os.Stderr, "main: HOME env variable is not present")
+	if !ok {
+		log.Print("HOME environment variable is not present. Exiting...")
 		os.Exit(1)
 	}
 	creds_path := filepath.Join(home_dir, ".aws/credentials")
@@ -72,23 +75,22 @@ func main() {
 	// parse config file
 	raw_file, err := ioutil.ReadFile(*config_path)
 	if err != nil {
-		fmt.Printf("main: %s\n", err)
+		log.Printf("Failed to read config file: %s", err)
 		os.Exit(1)
 	}
 	reminder_list := make([]reminder.ReminderV1, 0)
 	err = json.Unmarshal(raw_file, &reminder_list)
 	if err != nil {
-		fmt.Printf("main: %s\n", err)
+		log.Printf("Failed to parse config file: %s", err)
 		os.Exit(1)
 	}
-	fmt.Printf("parsed reminders: %v\n", reminder_list)
 
 	// main loop
 	var wait_time time.Duration = 60
-	ticker := time.NewTicker(wait_time*time.Second)
+	ticker := time.NewTicker(wait_time * time.Second)
 	for {
 		received_time := <-ticker.C
-		fmt.Printf("firing at time %s\n", received_time)
+		log.Print("checking reminders")
 		fire_reminders(received_time, sns_client, reminder_list)
 	}
 }

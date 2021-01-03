2238198a140e9c5be910eb5992fd30a2f88e34a6
Adam Pickering <adamkpickering@gmail.com>
2021-01-02 22:39:29 -0700

    Added documentation to types, functions etc in reminder/reminder.go

--- reminder/reminder.go
+++ reminder/reminder.go
@@ -6,27 +6,37 @@ import (
 	"encoding/json"
 )
 
+// The ShouldRun interface is implemented on types that contain data
+// that says when something should happen. A time (presumably the current
+// time) is passed to the ShouldRun method, and the type decides whether
+// its internal data matches that time.
 type ShouldRun interface {
 	ShouldRun(current_time time.Time) bool
 }
 
+// A Reminder is a single object that represents an even that you want
+// to be reminded of.
 type Reminder interface {
 	UnmarshalJSON([]byte) error
 	ShouldRun
 }
 
+// A Trigger is included as part of a Reminder. It is the part of the
+// Reminder that specifies when the message in the Reminder should be sent.
 type Trigger interface {
 	TriggerType() string
 	ParseTriggerFromInterfaceMap(map[string]interface{}) error
 	ShouldRun
 }
 
+// This is version 1 of the Reminder.
 type ReminderV1 struct {
 	Version  string
 	Message  string
 	Triggers []Trigger
 }
 
+// Determines whether r.Message should be sent.
 func (r *ReminderV1) ShouldRun(current_time time.Time) bool {
 	for _, trigger := range r.Triggers {
 		if trigger.ShouldRun(current_time) {
@@ -36,6 +46,7 @@ func (r *ReminderV1) ShouldRun(current_time time.Time) bool {
 	return false
 }
 
+// Unmarshals a []byte of data into a ReminderV1.
 func (r *ReminderV1) UnmarshalJSON(data []byte) error {
 	if string(data) == "null" { return nil }
 	obj := map[string]interface{}{}
@@ -89,6 +100,9 @@ func (r *ReminderV1) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// Determines the Trigger type, calls the appropriate code to parse the Trigger
+// part of the JSON into that type of Trigger, and then casts the resulting object
+// into the Trigger type.
 func parseTriggerFromInterface(obj_map map[string]interface{}) (Trigger, error) {
 	trigger_type, ok := obj_map["trigger_type"].(string)
 	if ! ok {
